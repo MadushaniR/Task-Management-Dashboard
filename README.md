458967fa66b8740e0552327bc2cae3b8cc1fcc26
MadushaniR <madushani.2019360@iit.ac.lk>
2024-09-15 00:43:17 +0530

    basic CRUD added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# TaskManagementDashboard
+# CrudApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.1.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Task-Management-Dashboard' title`, () => {
+  it(`should have as title 'crud-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Task-Management-Dashboard');
+    expect(app.title).toEqual('crud-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Task-Management-Dashboard');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-app app is running!');
   });
 });
